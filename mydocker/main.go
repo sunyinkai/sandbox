2b38d10f25eb70f53322e43f94af3d4ecf1e64ea
sunyinkai <1091491336@qq.com>
2020-01-17 10:40:50 +0800

    add compiler to FSM

--- mydocker/main.go
+++ mydocker/main.go
@@ -1,24 +1,58 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"path"
 	"syscall"
 )
 
+const cgroupMemoryHierarchyMount = "/sys/fs/cgroup/memory"
+
+func memoryTest() {
+	if os.Args[0] == "/proc/self/exe" {
+		fmt.Printf("current Pid is %d\n", syscall.Getpid())
+		cmd := exec.Command("sh", "-c", `stress --vm-bytes 200m --vm-keep -m 1`)
+		cmd.SysProcAttr = &syscall.SysProcAttr{}
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+}
 func main() {
-	cmd := exec.Command("sh")
+	memoryTest()
+	cmd := exec.Command("/proc/self/exe")
+
+	var namespaceFlags uintptr = syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID |
+		syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID |
-			syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
+		Cloneflags: namespaceFlags,
 	}
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	if err := cmd.Run(); err != nil {
+	if err := cmd.Start(); err != nil {
 		log.Fatal(err)
+	} else {
+		fmt.Printf("pid:%v", cmd.Process.Pid)
+		err := os.Mkdir(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit"), 0755)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		err = ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "memory.limit_in_bytes"), []byte("100m"),
+			0644)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
 	}
+	_, _ = cmd.Process.Wait()
 }
